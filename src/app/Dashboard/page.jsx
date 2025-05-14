7ce8508cf8a18cf8176e783e4842bd0f1052c9ef
Samrat <samrat@aruna.software>
2025-05-14 22:32:29 +0545

    ui changes in dashboard

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -2,6 +2,15 @@
 import React, { useEffect, useState } from 'react'
 import { useSession } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CheckCircle } from 'lucide-react';
 function page() {
   const {session} = useSession()
   const [websitelink, setWebsiteLink] = useState("")
@@ -34,20 +43,48 @@ function page() {
  
 
   return (
-    <div className="bg-gray-600 text-red-500 h-[32rem] p-4">
-      <input
-        type="text"
-        value={websitelink}
-        onChange={(e) => setWebsiteLink(e.target.value)}
-        className="p-2 rounded"
-        placeholder="Enter website"
-      />
-      <button
-        onClick={handleSubmit}
-        className="ml-2 p-2 bg-white text-black rounded"
-      >
-        Add Website
-      </button>
+    <div>
+      <h1 className=" mx-12 py-12 text-3xl font-semibold text-red-600">
+        Hi, User
+      </h1>
+      <div className=" grid grid-cols-1 place-items-center md:grid-cols-3">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium text-red-600 dark:text-red-500">
+              Online
+            </CardTitle>
+            <CheckCircle className="h-4 w-4 text-status-online" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">"GitGraph.com"</div>
+            <p className="text-xs text-muted-foreground">of 8 websites</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium text-red-600 dark:text-red-500">
+              Online
+            </CardTitle>
+            <CheckCircle className="h-4 w-4 text-status-online" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">"GitGraph.com"</div>
+            <p className="text-xs text-muted-foreground">of 8 websites</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium text-red-600 dark:text-red-500">
+              Online
+            </CardTitle>
+            <CheckCircle className="h-4 w-4 text-status-online" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">"GitGraph.com"</div>
+            <p className="text-xs text-muted-foreground">of 8 websites</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
